1f27e7b512623cf1b8cb580c6164ece7e8048e39
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-07 12:39:01 -0400

    Convert module to PDK Fix rubocop issues

--- spec/defines/root_ssh_authorized_key_spec.rb
+++ spec/defines/root_ssh_authorized_key_spec.rb
@@ -4,75 +4,68 @@ describe 'root::ssh_authorized_key' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts.merge({:puppetversion => Puppet.version})
+        facts.merge(puppetversion: Puppet.version)
       end
 
       let(:title) { 'foo' }
       let(:params) do
         {
-          :key  => 'longhash',
-          :type => 'rsa',
+          key: 'longhash',
+          type: 'rsa',
         }
       end
 
       it do
-        should contain_ssh_authorized_key('foo').only_with({
-          :ensure   => 'present',
-          :name     => 'foo',
-          :key      => 'longhash',
-          :type     => 'rsa',
-          :user     => 'root',
-        })
+        is_expected.to contain_ssh_authorized_key('foo').only_with(ensure: 'present',
+                                                                   name: 'foo',
+                                                                   key: 'longhash',
+                                                                   type: 'rsa',
+                                                                   user: 'root')
       end
 
       context 'when options defined' do
         let(:title) { 'foo' }
         let(:params) do
           {
-            :key      => 'longhash',
-            :options  => ['no-port-forwarding','no-pty'],
-            :type     => 'rsa',
+            key: 'longhash',
+            options: ['no-port-forwarding', 'no-pty'],
+            type: 'rsa',
           }
         end
 
         it do
-          should contain_ssh_authorized_key('foo').only_with({
-            :ensure   => 'present',
-            :name     => 'foo',
-            :key      => 'longhash',
-            :options  => ['no-port-forwarding','no-pty'],
-            :type     => 'rsa',
-            :user     => 'root',
-          })
+          is_expected.to contain_ssh_authorized_key('foo').only_with(ensure: 'present',
+                                                                     name: 'foo',
+                                                                     key: 'longhash',
+                                                                     options: ['no-port-forwarding', 'no-pty'],
+                                                                     type: 'rsa',
+                                                                     user: 'root')
         end
       end
 
       context 'when name is a key' do
-        let(:title) { "ssh-rsa longhash foo@bar" }
-        let(:params) {{}}
+        let(:title) { 'ssh-rsa longhash foo@bar' }
+        let(:params) { {} }
 
-        it { should create_root__ssh_authorized_key('ssh-rsa longhash foo@bar') }
+        it { is_expected.to create_root__ssh_authorized_key('ssh-rsa longhash foo@bar') }
 
         it do
-          should contain_ssh_authorized_key('ssh-rsa longhash foo@bar').only_with({
-            :ensure   => 'present',
-            :name     => 'foo@bar',
-            :key      => 'longhash',
-            :type     => 'ssh-rsa',
-            :user     => 'root',
-          })
+          is_expected.to contain_ssh_authorized_key('ssh-rsa longhash foo@bar').only_with(ensure: 'present',
+                                                                                          name: 'foo@bar',
+                                                                                          key: 'longhash',
+                                                                                          type: 'ssh-rsa',
+                                                                                          user: 'root')
         end
       end
 
       context 'when key and type are not defined' do
         let(:title) { 'foo' }
-        let(:params) {{}}
+        let(:params) { {} }
 
-        it "should raise error" do
-          expect { should compile }.to raise_error(/Unsupported namevar/)
+        it 'raises error' do
+          expect { is_expected.to compile }.to raise_error(%r{Unsupported namevar})
         end
       end
-
     end # end context
   end # end on_supported_os
 end # end describe
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,18 +4,20 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 dir = File.expand_path(File.dirname(__FILE__))
-Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each {|f| require f}
+Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each { |f| require f }
 
 run_puppet_install_helper
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 collection = ENV['BEAKER_PUPPET_COLLECTION'] || 'puppet5'
 if collection == 'puppet6'
-  on hosts, puppet('module','install','puppetlabs-mailalias_core')
-  on hosts, puppet('module','install','puppetlabs-sshkeys_core')
+  on hosts, puppet('module', 'install', 'puppetlabs-mailalias_core')
+  on hosts, puppet('module', 'install', 'puppetlabs-sshkeys_core')
 end
 
 RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
 end
+
+# 'spec_overrides' from sync.yml will appear below this line
