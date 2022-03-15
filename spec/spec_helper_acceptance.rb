2fd4d3563396daca0715cf32b65ec6b34deaf306
treydock <tdockendorf@osc.edu>
2022-03-15 09:57:48 -0400

    Major updates (Read description) (#13)
    * Drop EL6 support
    * Add EL8 support
    * Drop Puppet 5 support
    * Add Puppet 7 support
    * Update stdlib dependency version range
    
    Fixes #12

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -10,11 +10,6 @@ require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FI
 run_puppet_install_helper
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
-collection = ENV['BEAKER_PUPPET_COLLECTION'] || 'puppet5'
-if collection == 'puppet6'
-  on hosts, puppet('module', 'install', 'puppetlabs-mailalias_core')
-  on hosts, puppet('module', 'install', 'puppetlabs-sshkeys_core')
-end
 
 RSpec.configure do |c|
   # Readable test descriptions
